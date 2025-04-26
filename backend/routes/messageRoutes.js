e87fe240e057ea787cdf39f7e9220a4a1bff411a
emythri <eelapantimythri@gmail.com>
2025-04-26 09:30:07 +0530

    Changed structure of route

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -7,7 +7,10 @@ const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/:chatId").get(protect, allMessages);
+// router.route("/:chatId").get(protect, allMessages);
+
+router.get("/:chatId", protect, allMessages);
+
 router.route("/").post(protect, sendMessage);
 
 module.exports = router;
